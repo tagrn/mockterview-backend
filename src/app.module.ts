665ab82dd295b7583a963a82d794f5ab7d13aad3
tagrn <fksk94@gmail.com>
2023-09-25 13:44:53 +0900

    feat: database setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [QuestionsModule, UsersModule],
+  imports: [
+    QuestionsModule,
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DATABASE'),
+        entities: [],
+        synchronize: true,
+        charset: 'utf8mb4',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
