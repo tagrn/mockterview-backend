9a51ce5fcb6c067edfa7ddff6c91241eb36ec38a
tagrn <fksk94@gmail.com>
2023-10-05 14:19:15 +0900

    fix: user.nickname is not uniquekey

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,7 +11,6 @@ import {
 } from 'typeorm';
 
 @Index('email', ['email'], { unique: true })
-@Index('nickanme', ['nickname'], { unique: true })
 @Entity()
 export class User {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
